891bce7ea4024953f27dac96cda7f0c0b5f478ea
Jerry - li <lishuai4415@gmail.com>
2023-08-02 21:20:22 +0800

    init page

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Login: React.FC = () => {
+  return (
+    <></>
+  )
+}
+
+export default Login;
---
+++ src/pages/mine/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Mine: React.FC = () => {
+  return (
+    <></>
+  )
+}
+
+export default Mine;
